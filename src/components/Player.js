e56bb55af82dc778c1fc5c25bcddb3e6808a2d7a
Hank Yates <hankyates@gmail.com>
2021-01-17 22:17:53 -0800

    adding time start

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useRef, useState, useEffect} from 'react'
 import AudioPlayer from 'react-h5-audio-player'
 import 'react-h5-audio-player/lib/styles.css'
 
@@ -7,14 +7,21 @@ const episodes = Array(NUM_EPISODES).fill().map((item, index) => index + 1).reve
 
 export default function Player () {
   const [episode, setEpisode] = useState(3)
+  const player = useRef(null)
+
   useEffect(() => {
     let p = new URLSearchParams(window.location.search)
     if (p.has('episode') && episodes.includes(+p.get('episode'))) {
       setEpisode(p.get('episode'))
+      player.current.audio.current.currentTime
+    }
+    if (p.has('t')) {
+      player.current.audio.current.currentTime = +p.get('t')
     }
   }, [])
   return <div>
     <AudioPlayer
+      ref={player}
       src={`https://cdn.mousymagazine.org/episode-${episode}.mp3`}
       header={<h3>Currently Playing: Episode #{episode}</h3>}
       footer={
